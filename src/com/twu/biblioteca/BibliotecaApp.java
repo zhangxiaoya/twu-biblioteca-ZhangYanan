c771e73f97c75db62fb079c2cfe4d16df0bc0c54
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-08-02 23:14:56 +0800

    add Welcome when App start

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,16 @@
 package com.twu.biblioteca;
 
+import java.time.LocalDate;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String datestr = LocalDate.now().toString();
+        System.out.println("#####################################################");
+        System.out.println("                                                     ");
+        System.out.println("     Today is " + datestr + "                        ");
+        System.out.println("     Welcome to Online Bangalore Public Library      ");
+        System.out.println("                                                     ");
+        System.out.println("#####################################################");
     }
 }
