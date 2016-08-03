af390f291e006e8bc940f5f5eedff9d0212757cc
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-08-03 22:55:56 +0800

    extract method for init book sheet and print book list

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -9,13 +9,13 @@ public class BibliotecaApp {
         IWelcome welcomeGen = new Welcome();
         welcomeGen.printWelcomeMsg();
 
-        ArrayList<IBook> initBookList = new ArrayList<IBook>();
-        initBookList.add(new Book(1,"first book"));
-        initBookList.add(new Book(2,"second book"));
-        initBookList.add(new Book(3,"third book"));
-        BookSheet bookSheet = new BookSheet(initBookList);
+        BookSheet bookSheet = initBookSheet();
 
+        printBookList(bookSheet);
 
+    }
+
+    private static void printBookList(BookSheet bookSheet) {
         System.out.println("The Following is Book List: ");
         List<IBook> currentBookList = bookSheet.getBookList();
         for(int i=0;i<currentBookList.size();++i){
@@ -24,6 +24,13 @@ public class BibliotecaApp {
             System.out.print(((Book)currentBookList.get(i)).getName());
             System.out.println();
         }
+    }
 
+    private static BookSheet initBookSheet() {
+        ArrayList<IBook> initBookList = new ArrayList<IBook>();
+        initBookList.add(new Book(1,"first book"));
+        initBookList.add(new Book(2,"second book"));
+        initBookList.add(new Book(3,"third book"));
+        return new BookSheet(initBookList);
     }
 }
