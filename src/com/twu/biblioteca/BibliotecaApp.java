aff3d69a3a0e7a88a33a32f11b6e4cb06326d822
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-08-03 22:47:18 +0800

    after show welcome message, print all book info

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,9 +1,29 @@
 package com.twu.biblioteca;
 
+import java.util.List;
+import java.util.*;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         IWelcome welcomeGen = new Welcome();
         welcomeGen.printWelcomeMsg();
+
+        ArrayList<IBook> initBookList = new ArrayList<IBook>();
+        initBookList.add(new Book(1,"first book"));
+        initBookList.add(new Book(2,"second book"));
+        initBookList.add(new Book(3,"third book"));
+        BookSheet bookSheet = new BookSheet(initBookList);
+
+
+        System.out.println("The Following is Book List: ");
+        List<IBook> currentBookList = bookSheet.getBookList();
+        for(int i=0;i<currentBookList.size();++i){
+            System.out.print(((Book)currentBookList.get(i)).getId());
+            System.out.print("--");
+            System.out.print(((Book)currentBookList.get(i)).getName());
+            System.out.println();
+        }
+
     }
 }
--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -4,8 +4,25 @@ package com.twu.biblioteca;
  * Created by yanzhang on 8/2/16.
  */
 public class Book implements IBook {
+
     private long Id;
     private String Name;
+
+    public long getId(){
+        return Id;
+    }
+
+    public void setId(long id){
+        Id = id;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public void setName(String name){
+        Name = name;
+    }
 
     public Book(){
         Id = 0;
